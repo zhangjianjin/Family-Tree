f53ba115c2536a7a946426c3df9bbc3c5e09500f
zhangjj <zhangjj@zayagame.com>
2018-10-24 18:36:27 +0800

    收货地址

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://weibo.com/u/5499994695?is_hot=1" target="_blank">章健瑾</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:583247597@qq.com">联系我们</a></p>
+    </div>
+</footer>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PagesController@root')->name('root');
+Auth::routes();
+Route::group(['middleware' => 'auth'], function() {
+	Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+	Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+
+    // 开始
+    Route::group(['middleware' => 'email_verified'], function() {
+        Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+        Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+        Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+    });
+    // 结束
+});
