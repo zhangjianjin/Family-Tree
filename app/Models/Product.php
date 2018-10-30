bcc37399101624cd8f84c014a35fdd264737edde
583247597@qq.com <zhangjj@zaya.com>
2018-10-30 01:58:32 +0000

    商品模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+    	'title','description','image','on_sale',
+    	'rating','sold_count','review_count','price'
+    ];
+
+    protected $casts = [
+    	'on_sale' => 'boolean',
+    ];
+
+    public function skus()
+    {
+    	return $this-hasMany(ProductSku::class);
+    }
 }
