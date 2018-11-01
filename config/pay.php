195dc6412a2acc7d924f489ae7b6aa63c1b693d5
583247597@qq.com <zhangjj@zaya.com>
2018-11-01 10:33:49 +0000

    集成支付宝

--- config/pay.php
+++ config/pay.php
@@ -2,8 +2,8 @@
 
 return [
     'alipay' => [
-        'app_id'         => '',
-        'ali_public_key' => '',
+        'app_id'         => '2016092200566816',
+        'ali_public_key' => 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAseKIOypRKY3pT+u1dspLaQB8b0F9DEuqee2/rZT9QZKSJbjajco4zj5bWSL4FKghPWLOwYDWfieUgJHAxxJmqFmpXUWMxsOU9EKq5Xd4kln2JYgX5D4T22JNC98K4QWoaKtvFIi2ccIHaRxNtNVIuqTSxEiQeIBb+rCWG1FD5EvkKIsgTlOxgxf0qsUwYXMpXvf2JB1LJ0iCXao3SIJERjRjDnrZdmF9rzoT9poT4akQsxYKXD0jemD40frN7Ir0oTljyr9hUKLbDGUfGC/6VBe62wi7KxhfSx8NX9LIeJzng/yTImuN6Mgnk2cGjdmYuUIrEhd3F/tim87tSDGEowIDAQAB',
         'private_key'    => '',
         'log'            => [
             'file' => storage_path('logs/alipay.log'),
