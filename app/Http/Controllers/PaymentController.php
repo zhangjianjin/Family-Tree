d71f91b264e9b7c88034f8df726eaac839a8ea10
583247597@qq.com <zhangjj@zaya.com>
2018-11-09 07:58:12 +0000

    微信支付

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Endroid\QrCode\QrCode;
 use Carbon\Carbon;
 use App\Models\Order;
 use App\Exceptions\InvalodRequestException;
@@ -42,7 +43,7 @@ class PaymentController extends Controller
     	
     }
 
-    // 服务端回调
+    // 支付宝服务端回调
     public function alipayNotify()
     {
     	// 校验提交的参数是否合法
@@ -67,5 +68,55 @@ class PaymentController extends Controller
     	]);
     	
     	return app('alipay')->success();
+    }
+
+    public function payByWechat(Order $order, Request $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+        // 校验订单状态
+        if($order->paid_at || $order->closed){
+            throw new InvalidRequestException('订单状态不正确');
+        }
+
+        // scan方法为拉起微信扫码支付
+        $wechatOrder =  app('wechat_pay')->scan([
+            'out_trade_no' => $order->no,
+            'total_fee'    => $order->total_amount * 100,
+            'body'         => '支付 Family-Tree 的订单：'.$order->no, // 订单标题
+        ]);
+
+        //把要转化的字符串作为OrCode的构造函数参数
+        $qrCode = new QrCode($wechatOrder->code_url);
+
+        //将生产的二维码图片数据以字符串形式输出，并带上相应的相应类型
+        return response($qrCode->writeString(),200, ['Content-Type' => $qrCode->getContentType]);
+    }
+
+    // 微信服务端回调
+    public function wechatNotify()
+    {
+        // 校验回调参数是否正确
+        $data = app('wechat_pay')->verify();
+        // $data->out_trade_no 拿到订单流水号，并在数据库中查询
+        $order = Order::where('no', $data->out_trade_no)->first();
+        // 正常来说不太可能出现支付了一笔不存在的订单，这个判断只是加强系统的健壮性。
+        if(!$order){
+            return 'fail';
+        }
+
+        // 如果这笔订单的状态是已支付
+        if($order->paid_at){
+            // 返回数据给支付宝
+            return app('wechat_pay')->success();
+        }
+
+        $order->update([
+            'paid_at'       => Carbon::now(), //支付时间
+            'payment_method'=> 'wechat',
+            'payment_no'    => $data->transaction_id,
+        ]);
+        
+        return app('wechat_pay')->success();
     }
 }
