c75e6260875cb5e910962cef0fb64c92562cac4e
583247597@qq.com <zhangjj@zaya.com>
2018-11-19 08:49:29 +0000

    评价商品

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\OrderReviewed;
+use App\Listeners\UpdateProductRating;
 use App\Listeners\SendOrderPaidMail;
 use App\Events\OrderPaid;
 use App\Listeners\UpdateProductSoldCount;
@@ -30,6 +32,10 @@ class EventServiceProvider extends ServiceProvider
         OrderPaid::class => [
             UpdateProductSoldCount::class,
             SendOrderPaidMail::class,
+        ],
+
+        OrderReviewed::class => [
+            UpdateProductRating::class,
         ],
     ];
 
